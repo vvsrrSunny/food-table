70758464cc90fcd8dc49f4319279fb172890ca27
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-01-20 10:22:51 +0800

    added background

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <dashboard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from "./components/Dashboard.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Dashboard,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+<section></section>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+      body {
+        margin: 0;
+        font-family: 'Prompt', sans-serif;
+        color: white;
+        
+      }
+
+      section {
+        flex-direction: column;
+        align-items: center;
+        height: 250px;
+        color: #dddddd;
+        background: #354253;
+      }
+
+.styled-table thead tr {
+  background-color: #3b323f;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
